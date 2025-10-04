58d7003556951e8a7debfda3bbd7f2bad0a538d0
Benzema <164044166+Benzewa@users.noreply.github.com>
2025-10-04 15:28:34 +0000

    Up

---
+++ L-01/last_word.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+void last_word(char *str)
+{
+    int i = 0;
+    while(str[i])
+        i++;
+    i--;
+    while(str[i] && (str[i] == ' ' || str[i] == '\t'))
+        i--;
+    int end = i;
+    while(str[i] && (str[i] != ' ' && str[i] != '\t'))
+        i--;
+    int start = i + 1;
+    while(start <= end)
+    {
+        write(1,&str[start],1);
+        start++;
+    }
+}
+
+int main(int Ac, char **Av)
+{
+    if(Ac == 2)
+        last_word(Av[1]);
+    write(1,"\n",1);
+}
+
+// 4 October
