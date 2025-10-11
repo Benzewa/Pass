d4b2ef6c0051752aa2fd9a5b899f1520fef31ae9
Big Moe <bigmoe@motazmac.local>
2025-10-11 15:38:02 +0400

    Up

--- L-04/get_next_line/get_next_line.c
+++ L-04/get_next_line/get_next_line.c
@@ -1,11 +1,12 @@
 #include "get_next_line.h"
-
 char *get_next_line(int fd)
 {
   if (fd < 0 || BUFFER_SIZE <= 0)
     return (NULL);
+
   char *line = malloc(70000);
   int byte = read(fd, line, 70000);
+
   if (byte == 0)
   {
     free(line);
@@ -15,4 +16,4 @@ char *get_next_line(int fd)
   return (line);
 }
 
-// 11 October
+// 11 October 2025
