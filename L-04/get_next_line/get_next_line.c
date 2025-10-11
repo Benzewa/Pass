fb929d2046778b166b05e857947f2a2a41a50993
Big Moe <bigmoe@motazmac.local>
2025-10-11 13:43:11 +0400

    Up

--- L-04/get_next_line/get_next_line.c
+++ L-04/get_next_line/get_next_line.c
@@ -1,29 +1,18 @@
 #include "get_next_line.h"
 
-char *get_next_line(int fd) // lol its basically get_the_file
+char *get_next_line(int fd)
 {
-  int bytes;
-  char *line; //<--
-
   if (fd < 0 || BUFFER_SIZE <= 0)
     return (NULL);
-  line = malloc(1000000); //<--
-  bytes = read(fd, line, 1000000);
-  if (bytes <= 0)
-    return (free(line), NULL); //<--
-  line[bytes] = '\0';
+  char *line = malloc(70000);
+  int byte = read(fd, line, 70000);
+  if (byte == 0)
+  {
+    free(line);
+    return (NULL);
+  }
+  line[byte] = 0;
   return (line);
 }
 
-// int main()
-// {
-// 	int fd;
-//    	fd = open("test.txt",O_RDONLY);
-//     char *line;
-
-//    	while((line = get_next_line(fd)))
-//    	{
-//     	printf("%s",line);
-//    		free(line);
-//     }
-// }
+// 11 October
